d73a22167f87f7e05e3a8e6e3f98166c854b84f2
Shivansh Rai <shivanshrai84@gmail.com>
2016-12-09 23:44:55 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Build script
+
+trap kill_all SIGINT SIGTERM SIGHUP
+
+kill_all() {
+  echo "Cleaning up!!"
+  # for pid in $(jobs -p); do
+  for pid in ${job_pid[@]}; do
+    kill $pid
+  done
+  exit 0
+}
+
+bundle exec jekyll build --watch 1>/dev/null &
+job_pid[1]=$!
+bundle exec jekyll serve --incremental 1>/dev/null &
+job_pid[2]=$!
+sass --watch assets/_scss/main.scss:assets/css/main.css 1>/dev/null &
+job_pid[3]=$!
+
+echo "Starting build..."
+sleep 5
+echo "Jobs running. Visit http://localhost:4000."
+while true; do
+  read -p "Type (y) when done with testing: " yn;
+  case $yn in
+    y) kill_all;;
+    *) echo "Invalid argument!!";;
+  esac
+done
