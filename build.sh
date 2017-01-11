e7e0456f36f020b2a0df5b77a1b9e15d1905a11f
Shivansh Rai <shivansh@freebsd.org>
2017-01-11 20:11:55 +0530

    Refactor build script

--- build.sh
+++ build.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 # Build script
 
-trap kill_all SIGINT SIGTERM SIGHUP
+TIMEOUT=2
+trap cleanup SIGINT SIGTERM SIGHUP
 
-kill_all() {
-  echo "Cleaning up!!"
-  # for pid in $(jobs -p); do
-  for pid in ${job_pid[@]}; do
-    kill $pid
+cleanup() {
+  echo "⇒ Cleaning up!!"
+  for pid in "${job_pid[@]}"; do
+    kill "$pid"
   done
   exit 0
 }
 
-bundle exec jekyll build --watch > /dev/null &
-job_pid[1]=$!
-bundle exec jekyll serve --incremental > /dev/null &
-job_pid[2]=$!
-sass --watch assets/_scss/main.scss:assets/css/main.css > /dev/null &
-job_pid[3]=$!
+startBuild() {
+  bundle exec jekyll build --watch > /dev/null &
+  job_pid[1]=$!
+  bundle exec jekyll serve --incremental > /dev/null &
+  job_pid[2]=$!
+  sass --watch assets/_scss/main.scss:assets/css/main.css > /dev/null &
+  job_pid[3]=$!
+}
 
 echo "Starting build..."
-sleep 5
-echo "Jobs running. Visit http://localhost:4000."
+startBuild
+# Wait until all commands are up
+sleep $TIMEOUT
+echo -e "\e[1;32mJobs running. Visit http://localhost:4000\e[0m"
 while true; do
-  read -p "Type (y) when done with testing: " yn;
-  case $yn in
-    y) kill_all;;
-    *) echo "Invalid argument!!";;
-  esac
+  read -p "Exit? [Y] " response;
+  if [[ -z $response ]]; then
+    cleanup
+  fi
 done
