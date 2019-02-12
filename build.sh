f1135562145ab0b508c2145647714b33e1220c7d
Shivansh Rai <shivansh@freebsd.org>
2019-02-12 22:11:51 +0530

    build: poll the background processes to confirm successful initiation

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 set -eu
 
-TIMEOUT=4
+TIMEOUT=2  # approximate time required by jekyll {build, serve}
 trap cleanup SIGINT SIGTERM SIGHUP
 
 cleanup() {
@@ -16,13 +16,25 @@ cleanup() {
 
 startBuild() {
   bundle exec jekyll build --watch &> /dev/null &
+  sleep $TIMEOUT
+  # Poll the background process to check if it was successfully started.
+  if ! kill -0 $!; then
+    echo "command failed: jekyll build"
+    cleanup 1
+  fi
   job_pid[1]=$!
+
   bundle exec jekyll serve --incremental &> /dev/null &
+  sleep $TIMEOUT
+  if ! kill -0 $!; then
+    echo "command failed: jekyll serve"
+    cleanup 1
+  fi
   job_pid[2]=$!
   # TODO: Error while installing ffi gem. For now, avoid using '--watch' flag.
   # sass --watch assets/_scss/main.scss:assets/css/main.css &> /dev/null &
+  #   echo "command failed: sass"
   # job_pid[3]=$!
-  sleep $TIMEOUT
 }
 
 echo "Starting build..."
